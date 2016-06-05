47a3b4fab9cb09d9320c1da05f7513e613a54b4b
AvinashKumarSP <spavinash1989@gmail.com>
2016-06-05 19:44:53 +0530

    Update AuthConstants.java

--- src/main/java/com/example/twitter/AuthConstants.java
+++ src/main/java/com/example/twitter/AuthConstants.java
@@ -5,9 +5,9 @@ public class AuthConstants {
 
 	private AuthConstants() { }
 	
-	public static final String consumerKey = "jdzdMFL4iTudHdwJCNhCVOtwZ";
-	public static final String consumerSecret = "dfyAVuKPcNLsK3nXTKKeYzARwu5g7DYuRfFW9Ti1i4ep7HE3SO";
-	public static final String token = "891087234-k3nKb8XSObABcIXdQpfdcv0WYNYNdsoHlMoajCHc";
-	public static final String secret = "GgzPRSXyFjhoRIco33dGM6K5kS0HCeNHlHRdccLs4M486";
+	public static final String consumerKey = <Consumer Key>;
+	public static final String consumerSecret = <Consumer Secret>;
+	public static final String token = <Token>;
+	public static final String secret = <Secret>;
 
 }
